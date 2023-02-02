e48481aa29fc57cc46e57bceff6856ebdcffff23
germanlasserre <germanlasserre@gmail.com>
2023-02-02 04:23:14 -0300

    actualizo

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,13 +7,4 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index); 
 
-//miflash
-//router.get('/detail', mainController.detail); 
-//router.get('/error', mainController.error); 
-//router.get('/product-create-form', mainController.create);
-//router.get('/product-edit-form', mainController.edit);
-//router.get('/products', mainController.products);
-//router.get('/results', mainController.results);
-//hasta aca
-
 module.exports = router;
